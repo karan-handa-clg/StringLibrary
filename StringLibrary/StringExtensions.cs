b1eff4ea92d3449ddf26602dfd0fe4c166296032
karan-handa-clg <khanda4@my.centennialcollege.ca>
2025-04-04 17:06:01 -0400

    Removed .vs folder and committed necessary files

---
+++ StringLibrary/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringLibrary
+{
+    public static class StringExtension
+    {
+        // Extension method to check if string starts with an uppercase letter
+        public static bool StartsWithUpperCase(this string str)
+        {
+            // Check if the string is null or empty
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            // Check if the first character is uppercase
+            return Char.IsUpper(str[0]);
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using StringLibrary;  // Import the StringLibrary project
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Test the extension method
+        string test1 = "Hello World";
+        string test2 = "hello World";
+
+        Console.WriteLine(test1.StartsWithUpperCase());  // Expected: True
+        Console.WriteLine(test2.StartsWithUpperCase());  // Expected: False
+    }
+}
